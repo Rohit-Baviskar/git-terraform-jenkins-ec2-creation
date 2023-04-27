9011588d6f269bfd0894f87f57bd09a626d56dd6
Rohit-Baviskar <90634897+Rohit-Baviskar@users.noreply.github.com>
2023-04-27 13:07:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "ec2-user" {
-  ami           = "ami-0f8ca728008ff5af4" 
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-02eb7a4783e7e9317"  
   instance_type = "t2.micro"
   tags = {
       Name = "infra-server"
